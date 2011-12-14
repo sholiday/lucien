cbec38531654d15b0b99450eafca2c902435c7ce
Stephen Holiday <stephen.holiday@gmail.com>
2011-12-14 01:29:29 -0500

    Now with a basic python interpreter

--- src/main/java/com/riffbit/lucien/App.java
+++ src/main/java/com/riffbit/lucien/App.java
@@ -7,16 +7,28 @@ import com.riffbit.lucien.bucket.StoreBucket;
 import com.riffbit.lucien.store.Store;
 import com.riffbit.lucien.store.LevelDBStore;
 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        Store store = new LevelDBStore("soemthing","/tmp");
-        Bucket bk = new StoreBucket(store);
-        
-        JSONObject j = new JSONObject();
-        j.put("k","v");
-        bk.put("a", j);
-        System.out.println(bk.get("a"));
-    }
+	public static void main( String[] args )
+	{
+		Store store = new LevelDBStore("soemthing","/tmp");
+		Bucket bk = new StoreBucket(store);
+
+		JSONObject j = new JSONObject();
+		j.put("k","v");
+		bk.put("a", j);
+		System.out.println(bk.get("a"));
+
+		try {
+			ScriptEngine engine = new ScriptEngineManager().getEngineByName("python");
+			engine.eval("print \"hello world\"");
+		} catch (ScriptException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
